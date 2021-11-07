05038172f7858f16d7600ae5cd75905a04ebabc0
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-07 22:36:43 +0200

    home page created class and PODO with data to use

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,87 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Welcome Home'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          iconTheme: IconThemeData(color: Colors.white),
+          elevation: 0,
+          title: Center(
+            child: Icon(
+              Icons.pool,
+              color: Colors.white,
+            ),
+          ),
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 15),
+              child: Icon(
+                Icons.notifications,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: mainThemeColor,
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50),
+              topRight: Radius.circular(50),
+            ),
+          ),
+        ));
   }
 }
+
+class Attraction {
+  String? imgPath;
+  String? name;
+  String? desc;
+  double? price;
+  String? location;
+  int? rating;
+
+  Attraction(
+      {this.imgPath,
+      this.name,
+      this.desc,
+      this.price,
+      this.location,
+      this.rating});
+}
+
+final List<Attraction> attractionsList = [
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/275162028.jpg?k=38b638c8ec9ec86624f9a598482e95fa634d49aa3f99da1838cf5adde1a14521&o=&hp=1',
+      name: 'Grand Bavaro Princess',
+      desc: 'All-Inclusive Resort',
+      location: 'Punta Cana, DR',
+      rating: 3,
+      price: 80.0),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/232161008.jpg?k=27808fe44ab95f6468e5433639bf117032c8271cebf5988bdcaa0a202b9a6d79&o=&hp=1',
+      name: 'Hyatt Ziva Cap Cana',
+      desc: 'All-Inclusive Resort',
+      price: 90.0,
+      rating: 4,
+      location: 'Punta Cana, DR'),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/256931299.jpg?k=57b5fb9732cd89f308def5386e221c46e52f48579345325714a310addf819274&o=&hp=1',
+      name: 'Impressive Punta Cana',
+      desc: 'All-Inclusive Resort',
+      price: 100.0,
+      rating: 5,
+      location: 'Punta Cana, DR'),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/283750757.jpg?k=4f3437bf1e1b077463c9900e4dd015633db1d96da38f034f4b70a4ba3ef76d82&o=&hp=1',
+      name: 'Villas Mar Azul Dreams',
+      desc: 'All-Inclusive Resort',
+      price: 100.0,
+      rating: 4,
+      location: 'Tallaboa, PR'),
+];
