f04a3badce3246a5d770582aeadc63096f2c2a33
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-07 22:46:38 +0200

    home page returned data from podo

--- lib/main.dart
+++ lib/main.dart
@@ -47,36 +47,50 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(color: Colors.white),
-          elevation: 0,
-          title: Center(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white),
+        elevation: 0,
+        title: Center(
+          child: Icon(
+            Icons.pool,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 15),
             child: Icon(
-              Icons.pool,
+              Icons.notifications,
               color: Colors.white,
             ),
+          )
+        ],
+      ),
+      backgroundColor: mainThemeColor,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
           ),
-          actions: [
-            Container(
-              margin: EdgeInsets.only(right: 15),
-              child: Icon(
-                Icons.notifications,
-                color: Colors.white,
-              ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: attractionsList.length,
+                  itemBuilder: (context, index) {
+                    Attraction attr = attractionsList[index];
+                    return Text(attr.name!);
+                  }),
             )
           ],
         ),
-        backgroundColor: mainThemeColor,
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(50),
-              topRight: Radius.circular(50),
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
 
