b9a009a3610ba8cb29c20a1988ea9a91b53763ab
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-08 09:17:52 +0200

    rendered images

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 
 const Color mainThemeColor = Color(0xFF272D8D);
+
+final List<Attraction> attractionsList = [
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/275162028.jpg?k=38b638c8ec9ec86624f9a598482e95fa634d49aa3f99da1838cf5adde1a14521&o=&hp=1',
+      name: 'Grand Bavaro Princess',
+      desc: 'All-Inclusive Resort',
+      location: 'Punta Cana, DR',
+      rating: 3,
+      price: 80.0),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/232161008.jpg?k=27808fe44ab95f6468e5433639bf117032c8271cebf5988bdcaa0a202b9a6d79&o=&hp=1',
+      name: 'Hyatt Ziva Cap Cana',
+      desc: 'All-Inclusive Resort',
+      price: 90.0,
+      rating: 4,
+      location: 'Punta Cana, DR'),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/256931299.jpg?k=57b5fb9732cd89f308def5386e221c46e52f48579345325714a310addf819274&o=&hp=1',
+      name: 'Impressive Punta Cana',
+      desc: 'All-Inclusive Resort',
+      price: 100.0,
+      rating: 5,
+      location: 'Punta Cana, DR'),
+  Attraction(
+      imgPath:
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/283750757.jpg?k=4f3437bf1e1b077463c9900e4dd015633db1d96da38f034f4b70a4ba3ef76d82&o=&hp=1',
+      name: 'Villas Mar Azul Dreams',
+      desc: 'All-Inclusive Resort',
+      price: 100.0,
+      rating: 4,
+      location: 'Tallaboa, PR'),
+];
+
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage()));
 }
@@ -84,7 +120,9 @@ class ListPage extends StatelessWidget {
                   itemCount: attractionsList.length,
                   itemBuilder: (context, index) {
                     Attraction attr = attractionsList[index];
-                    return Text(attr.name!);
+                    return AttractionCard(
+                      attraction: attr,
+                    );
                   }),
             )
           ],
@@ -99,7 +137,26 @@ class AttractionCard extends StatelessWidget {
   AttractionCard({this.attraction});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 300,
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(attraction!.imgPath!),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              Container(),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -119,38 +176,3 @@ class Attraction {
       this.location,
       this.rating});
 }
-
-final List<Attraction> attractionsList = [
-  Attraction(
-      imgPath:
-          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/275162028.jpg?k=38b638c8ec9ec86624f9a598482e95fa634d49aa3f99da1838cf5adde1a14521&o=&hp=1',
-      name: 'Grand Bavaro Princess',
-      desc: 'All-Inclusive Resort',
-      location: 'Punta Cana, DR',
-      rating: 3,
-      price: 80.0),
-  Attraction(
-      imgPath:
-          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/232161008.jpg?k=27808fe44ab95f6468e5433639bf117032c8271cebf5988bdcaa0a202b9a6d79&o=&hp=1',
-      name: 'Hyatt Ziva Cap Cana',
-      desc: 'All-Inclusive Resort',
-      price: 90.0,
-      rating: 4,
-      location: 'Punta Cana, DR'),
-  Attraction(
-      imgPath:
-          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/256931299.jpg?k=57b5fb9732cd89f308def5386e221c46e52f48579345325714a310addf819274&o=&hp=1',
-      name: 'Impressive Punta Cana',
-      desc: 'All-Inclusive Resort',
-      price: 100.0,
-      rating: 5,
-      location: 'Punta Cana, DR'),
-  Attraction(
-      imgPath:
-          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/283750757.jpg?k=4f3437bf1e1b077463c9900e4dd015633db1d96da38f034f4b70a4ba3ef76d82&o=&hp=1',
-      name: 'Villas Mar Azul Dreams',
-      desc: 'All-Inclusive Resort',
-      price: 100.0,
-      rating: 4,
-      location: 'Tallaboa, PR'),
-];
