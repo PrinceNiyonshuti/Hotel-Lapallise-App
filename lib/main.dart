dbdf2e3f5d913d09d0805e37fc1cf8ce835e74b2
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-08 09:32:45 +0200

    rendered rating with stars

--- lib/main.dart
+++ lib/main.dart
@@ -184,6 +184,9 @@ class AttractionCard extends StatelessWidget {
                                   color: Colors.grey.withOpacity(0.7)),
                             )
                           ],
+                        ),
+                        RatingWidget(
+                          rating: attraction!.rating!,
                         )
                       ],
                     ),
@@ -197,6 +200,26 @@ class AttractionCard extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class RatingWidget extends StatelessWidget {
+  int? rating;
+  RatingWidget({this.rating});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Row(
+          children: List.generate(5, (index) {
+            return Icon(
+              index < this.rating! ? Icons.star : Icons.star_border,
+              color: Colors.yellow,
+            );
+          }),
+        )
+      ],
     );
   }
 }
