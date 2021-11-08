b675205cde29a0067e367f75b703b8f6f2050e8e
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-08 13:12:28 +0200

    new layout card and shadow done

--- lib/main.dart
+++ lib/main.dart
@@ -138,118 +138,134 @@ class AttractionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: Stack(
-        children: [
-          Column(
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              offset: Offset.zero,
+              color: Colors.black.withOpacity(0.1),
+            ),
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(40),
+        child: Container(
+          height: 300,
+          child: Stack(
             children: [
-              Container(
-                height: 150,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(attraction!.imgPath!),
-                      fit: BoxFit.cover),
-                ),
-              ),
-              Container(
-                height: 150,
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              Column(
+                children: [
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(attraction!.imgPath!),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Container(
+                    height: 150,
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          attraction!.name!,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Row(
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.pin_drop,
-                              color: Colors.grey.withOpacity(0.7),
-                              size: 13,
+                            Text(
+                              attraction!.name!,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                             SizedBox(
                               width: 5,
                             ),
-                            Text(
-                              attraction!.location!,
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey.withOpacity(0.7)),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.pin_drop,
+                                  color: Colors.grey.withOpacity(0.7),
+                                  size: 13,
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  attraction!.location!,
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey.withOpacity(0.7)),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            RatingWidget(
+                              rating: attraction!.rating!,
                             )
                           ],
                         ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        RatingWidget(
-                          rating: attraction!.rating!,
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          '\$${attraction!.price!.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          'Per Night',
-                          style: TextStyle(
-                            color: Colors.grey.withOpacity(0.7),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              '\$${attraction!.price!.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Per Night',
+                              style: TextStyle(
+                                color: Colors.grey.withOpacity(0.7),
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  margin: EdgeInsets.only(right: 10),
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: mainThemeColor,
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            color: mainThemeColor.withOpacity(0.5),
+                            offset: Offset.zero)
+                      ]),
+                ),
+              )
             ],
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              child: Icon(
-                Icons.favorite,
-                color: Colors.white,
-                size: 16,
-              ),
-              margin: EdgeInsets.only(right: 10),
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                  color: mainThemeColor,
-                  borderRadius: BorderRadius.circular(40),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 10,
-                        color: mainThemeColor.withOpacity(0.5),
-                        offset: Offset.zero)
-                  ]),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
